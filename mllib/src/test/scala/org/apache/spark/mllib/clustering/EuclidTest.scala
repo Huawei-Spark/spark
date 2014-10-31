8570c37acc024c4dde943d8887416525a12157f7
sboeschhuawei <stephen.boesch@huawei.com>
2014-10-31 12:42:46 -0700

    poc for Gaussian sim

--- mllib/src/test/scala/org/apache/spark/mllib/clustering/EuclidTest.scala
+++ mllib/src/test/scala/org/apache/spark/mllib/clustering/EuclidTest.scala
@@ -1,3 +1,5 @@
+package org.apache.spark.mllib.clustering
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -14,33 +16,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.spark.mllib.clustering
-
-
-import org.apache.log4j.Logger
-import org.apache.spark.SparkContext
-import org.apache.spark.mllib.linalg.{Matrix, Matrices, Vectors}
-import org.apache.spark.mllib.linalg.distributed.RowMatrix
-import breeze.linalg.{DenseVector => BDV, DenseMatrix => BDM}
-import org.apache.spark.mllib.rdd.RDDFunctions
-import scala.math.Ordering
-import org.apache.spark.mllib.rdd.RDDFunctions._
 
 /**
- * SpectralClustering
+ * EuclidTest
+ *
  */
-class SpectralClustering {
-  val logger = Logger.getLogger(getClass.getName)
-
-}
-
-object SpectralClustering {
-
-  val log = Logger.getLogger(SpectralClustering.getClass)
-
-//  def gaussians(nPoints: Int, mean: Double, sttdev: Double) = {
-//    import org.apache.spark.mllib.stat.Statistics
-//
-//  }
+class EuclidTest {
 
 }
