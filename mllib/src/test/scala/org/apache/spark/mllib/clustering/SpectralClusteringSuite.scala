abe1eba21a05b42b824cfb08bc7ad908c2c5326a
sboeschhuawei <stephen.boesch@huawei.com>
2014-11-12 22:47:22 -0800

    Fix accumulator mult instead of add bug

--- mllib/src/test/scala/org/apache/spark/mllib/clustering/SpectralClusteringSuite.scala
+++ mllib/src/test/scala/org/apache/spark/mllib/clustering/SpectralClusteringSuite.scala
@@ -1,4 +1,3 @@
-package org.apache.spark.mllib.clustering
 /*
 * Licensed to the Apache Software Foundation (ASF) under one or more
 * contributor license agreements. See the NOTICE file distributed with
@@ -15,6 +14,7 @@ package org.apache.spark.mllib.clustering
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
+package org.apache.spark.mllib.clustering
 import org.apache.spark.mllib.linalg.distributed.{IndexedRow, IndexedRowMatrix}
 import org.apache.spark.mllib.util.LocalSparkContext
 import org.apache.spark.rdd.{ZippedWithIndexRDDPartition, ParallelCollectionPartition, RDD}
