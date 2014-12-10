25118b6562ca8c6abdea63e4600b6197caadba16
sboeschhuawei <stephen.boesch@huawei.com>
2014-12-09 16:05:55 -0800

    Updates to join and aggregates testcases

--- sql/hbase/src/test/scala/org/apache/spark/sql/hbase/AggregateQueriesSuite.scala
+++ sql/hbase/src/test/scala/org/apache/spark/sql/hbase/AggregateQueriesSuite.scala
@@ -24,21 +24,38 @@ class AggregateQueriesSuite extends QueriesSuiteBase {
   private val logger = Logger.getLogger(getClass.getName)
 
   var testnm = "Group by with cols in select list and with order by"
-  test(testnm) {
-    val query1 =
+  test("Group by with cols in select list and with order by") {
+       val query =
       s"""select count(1) as cnt, intcol, floatcol, strcol, max(bytecol) bytecol, max(shortcol) shortcol,
          | max(floatcol) floatcolmax, max(doublecol) doublecol, max(longcol) from $tabName
          |  where strcol like '%Row%' and shortcol < 12345 and doublecol > 5678912.345681
          |  and doublecol < 5678912.345684
          | group by intcol, floatcol, strcol order by strcol desc"""
-        .stripMargin
 
-    val execQuery1 = hbc.executeSql(query1)
+    testGroupBy(testnm, query)
+  }
+
+    testnm = "Group by with cols in select list and with having and order by"
+  test("Group by with cols in select list and with having and order by") {
+    val query = s"""select count(1) as cnt, intcol, floatcol, strcol, max(bytecol) bytecolmax,
+         max(shortcol) shortcolmax, max(floatcol) floatcolmax, max(doublecol) doublecolmax,
+          max(longcol) longcolmax
+          from $tabName
+          where strcol like '%Row%' and shortcol < 12345 and doublecol > 5678912.345681
+          and doublecol < 5678912.345685
+         group by intcol, floatcol, strcol
+         having max(doublecol) < 5678912.345684
+         order by strcol desc""".stripMargin
+    testGroupBy(testnm, query)
+  }
+
+  def testGroupBy(testName: String, query: String) = {
+    val execQuery1 = hbc.executeSql(query)
     val result1 = execQuery1.toRdd.collect()
-    assert(result1.size == 2, s"$testnm failed on size")
+    assert(result1.size == 2, s"$testName failed on size")
     val exparr = Array(
-      Array(1,23456783, 45657.83F, "Row3", 'c', 12343, 45657.83F, 5678912.345683, 3456789012343L),
-      Array(1,23456782, 45657.82F, "Row2", 'b', 12342, 45657.82F, 5678912.345682, 3456789012342L))
+      Array(1, 23456783, 45657.83F, "Row3", 'c', 12343, 45657.83F, 5678912.345683, 3456789012343L),
+      Array(1, 23456782, 45657.82F, "Row2", 'b', 12342, 45657.82F, 5678912.345682, 3456789012342L))
 
     var res = {
       for (rx <- 0 until exparr.size)
@@ -46,14 +63,14 @@ class AggregateQueriesSuite extends QueriesSuiteBase {
     }.foldLeft(true) { case (res1, newres) => res1 && newres}
     assert(res, "One or more rows did not match expected")
 
-    println(s"$query1 came back with ${result1.size} results")
+    println(s"$query came back with ${result1.size} results")
     println(result1.mkString)
 
-    println(s"Test $testnm completed successfully")
+    println(s"Test $testName completed successfully")
   }
 
-  testnm = "Group by with cols in select list and with having and order by"
-  test(testnm) {
+  testnm = "Another Group by with cols in select list and with having and order by"
+  test("Another Group by with cols in select list and with having and order by") {
     val query1 =
       s"""select count(1) as cnt, intcol, floatcol, strcol, max(bytecol) bytecol, max(shortcol) shortcol,
          | max(floatcol) floatcolmax, max(doublecol) doublecol, max(longcol) from $tabName
@@ -66,8 +83,8 @@ class AggregateQueriesSuite extends QueriesSuiteBase {
     val result1 = execQuery1.toRdd.collect()
     assert(result1.size == 2, s"$testnm failed on size")
     val exparr = Array(
-      Array(1,23456783, 45657.83F, "Row3", 'c', 12343, 45657.83F, 5678912.345683, 3456789012343L),
-      Array(1,23456782, 45657.82F, "Row2", 'b', 12342, 45657.82F, 5678912.345682, 3456789012342L))
+      Array(1, 23456783, 45657.83F, "Row3", 'c', 12343, 45657.83F, 5678912.345683, 3456789012343L),
+      Array(1, 23456782, 45657.82F, "Row2", 'b', 12342, 45657.82F, 5678912.345682, 3456789012342L))
 
     var res = {
       for (rx <- 0 until exparr.size)
