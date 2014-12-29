06478c73088570bcc5e1859b5c1d6fab946d26c9
Bo Meng <bo.meng@huawei.com>
2014-12-29 14:49:13 -0800

    add test case for computePredicate in HBasePartition

--- sql/hbase/src/test/scala/org/apache/spark/sql/hbase/HBasePartitionerSuite.scala
+++ sql/hbase/src/test/scala/org/apache/spark/sql/hbase/HBasePartitionerSuite.scala
@@ -17,11 +17,16 @@
 
 package org.apache.spark.sql.hbase
 
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.hbase.HBaseConfiguration
+import org.apache.spark.{SparkContext, SparkConf}
 import org.apache.spark.sql.catalyst.expressions._
 import org.apache.spark.sql.catalyst.types._
 import org.scalatest.FunSuite
 import org.apache.hadoop.hbase.util.Bytes
 import org.apache.spark.rdd.ShuffledRDD
+
+import scala.collection.mutable.ArrayBuffer
 
 class HBasePartitionerSuite extends FunSuite with HBaseTestSparkContext {
   test("test hbase partitioner") {
@@ -112,5 +117,96 @@ class HBasePartitionerSuite extends FunSuite with HBaseTestSparkContext {
     assert(BytesUtils.toDouble(rowkey, keys(0)._1) === 123.456)
     assert(BytesUtils.toString(rowkey, keys(1)._1, keys(1)._2) === "abcdef")
     assert(BytesUtils.toInt(rowkey, keys(2)._1) === 1234)
+  }
+
+  test("test computePredicate in HBasePartition") {
+    val namespace = "testNamespace"
+    val tableName = "testTable"
+    val hbaseTableName = "ht"
+    val family1 = "family1"
+    val family2 = "family2"
+
+    val hbaseContext = new HBaseSQLContext(sparkContext)
+    val catalog = new HBaseCatalog(hbaseContext)
+    val configuration = HBaseConfiguration.create()
+
+    var allColumns = List[AbstractColumn]()
+    allColumns = allColumns :+ KeyColumn("column1", IntegerType, 0)
+    allColumns = allColumns :+ KeyColumn("column2", IntegerType, 1)
+    allColumns = allColumns :+ NonKeyColumn("column3", FloatType, family2, "qualifier2")
+    allColumns = allColumns :+ NonKeyColumn("column4", BooleanType, family1, "qualifier1")
+
+    val relation = HBaseRelation(tableName, namespace, hbaseTableName, allColumns)(hbaseContext)
+
+    val lll = relation.output.find(_.name == "column2").get
+    val llr = Literal(8, IntegerType)
+    val ll = EqualTo(lll, llr)
+
+    val lrl = lll
+    val lrr = Literal(2048, IntegerType)
+    val lr = EqualTo(lrl, lrr)
+
+    val l = Or(ll, lr)
+
+    val rll = relation.output.find(_.name == "column1").get
+    val rlr = Literal(32, IntegerType)
+    val rl = EqualTo(rll, rlr)
+
+    val rrl = rll
+    val rrr = Literal(1024, IntegerType)
+    val rr = EqualTo(rrl, rrr)
+
+    val r = Or(rl, rr)
+
+    val mid = And(l, r)
+    val pred = Some(mid)
+
+    val result = RangeCriticalPoint.generateCriticalPointRanges(relation, pred)
+
+    assert(result.size == 2)
+
+    val expandedCPRs: Seq[MDCriticalPointRange[_]] =
+      result.flatMap(_.flatten(new ArrayBuffer[(Any, NativeType)](relation.dimSize)))
+
+    assert(expandedCPRs.size == 4)
+
+    val rowkey0 = HBaseKVHelper.encodingRawKeyColumns(
+      Seq((BytesUtils.create(IntegerType).toBytes(1), IntegerType)
+        , (BytesUtils.create(IntegerType).toBytes(1), IntegerType))
+    )
+
+    val rowkey1 = HBaseKVHelper.encodingRawKeyColumns(
+      Seq((BytesUtils.create(IntegerType).toBytes(8), IntegerType)
+        , (BytesUtils.create(IntegerType).toBytes(2), IntegerType))
+    )
+
+    val rowkey2 = HBaseKVHelper.encodingRawKeyColumns(
+      Seq((BytesUtils.create(IntegerType).toBytes(32), IntegerType)
+        , (BytesUtils.create(IntegerType).toBytes(16), IntegerType))
+    )
+
+    val rowkey3 = HBaseKVHelper.encodingRawKeyColumns(
+      Seq((BytesUtils.create(IntegerType).toBytes(64), IntegerType)
+        , (BytesUtils.create(IntegerType).toBytes(128), IntegerType))
+    )
+
+    val rowkey4 = HBaseKVHelper.encodingRawKeyColumns(
+      Seq((BytesUtils.create(IntegerType).toBytes(1024), IntegerType)
+        , (BytesUtils.create(IntegerType).toBytes(256), IntegerType))
+    )
+
+    val p1 = new HBasePartition(0, 0, None, Some(rowkey0), None, pred, relation)
+    val p2 = new HBasePartition(1, 1, Some(rowkey0), Some(rowkey1), None, pred, relation)
+    val p3 = new HBasePartition(2, 2, Some(rowkey1), Some(rowkey2), None, pred, relation)
+    val p4 = new HBasePartition(3, 3, Some(rowkey2), Some(rowkey3), None, pred, relation)
+    val p5 = new HBasePartition(4, 4, Some(rowkey3), Some(rowkey4), None, pred, relation)
+    val p6 = new HBasePartition(5, 5, Some(rowkey4), None, None, pred, relation)
+
+    relation.partitions = Seq(p1, p2, p3, p4, p5, p6)
+
+    for (partition <- relation.partitions) {
+      val predicate = partition.computePredicate(relation)
+      println(predicate)
+    }
   }
 }
