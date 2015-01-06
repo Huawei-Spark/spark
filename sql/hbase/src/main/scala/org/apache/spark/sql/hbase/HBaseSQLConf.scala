29af728a0d002eb610301d074db64fa34a0db2d7
xinyunh <xinyun.huang@huawei.com>
2015-01-05 18:47:12 -0800

    Fix the issue in HBaseSQLParser and Add more testcases

--- sql/hbase/src/main/scala/org/apache/spark/sql/hbase/HBaseSQLConf.scala
+++ sql/hbase/src/main/scala/org/apache/spark/sql/hbase/HBaseSQLConf.scala
@@ -21,6 +21,28 @@ import org.apache.spark.sql.SQLConf
 
 private[hbase] object HBaseSQLConf {
   val PARTITION_EXPIRATION = "spark.sql.hbase.partition"
+  val COMPRESS_CACHED = "spark.sql.inMemoryColumnarStorage.compressed"
+  val COLUMN_BATCH_SIZE = "spark.sql.inMemoryColumnarStorage.batchSize"
+  val IN_MEMORY_PARTITION_PRUNING = "spark.sql.inMemoryColumnarStorage.partitionPruning"
+  val AUTO_BROADCASTJOIN_THRESHOLD = "spark.sql.autoBroadcastJoinThreshold"
+  val DEFAULT_SIZE_IN_BYTES = "spark.sql.defaultSizeInBytes"
+  val SHUFFLE_PARTITIONS = "spark.sql.shuffle.partitions"
+  val CODEGEN_ENABLED = "spark.sql.codegen"
+  val DIALECT = "spark.sql.dialect"
+
+  val PARQUET_BINARY_AS_STRING = "spark.sql.parquet.binaryAsString"
+  val PARQUET_CACHE_METADATA = "spark.sql.parquet.cacheMetadata"
+  val PARQUET_COMPRESSION = "spark.sql.parquet.compression.codec"
+  val PARQUET_FILTER_PUSHDOWN_ENABLED = "spark.sql.parquet.filterPushdown"
+
+  val COLUMN_NAME_OF_CORRUPT_RECORD = "spark.sql.columnNameOfCorruptRecord"
+
+  // Options that control which operators can be chosen by the query planner.  These should be
+  // considered hints and may be ignored by future versions of Spark SQL.
+  val EXTERNAL_SORT = "spark.sql.planner.externalSort"
+
+  // This is only used for the thriftserver
+  val THRIFTSERVER_POOL = "spark.sql.thriftserver.scheduler.pool"
 }
 
 /**
