439b3ef84dd631d97286699062d95ade821cd567
sboeschhuawei <stephen.boesch@huawei.com>
2015-01-09 19:16:50 -0800

    Fixing eigenvalue/vector computation bugs and added data validations

--- graphx/src/test/scala/org/apache/spark/graphx/SpectralClusteringSuite.scala
+++ graphx/src/test/scala/org/apache/spark/graphx/SpectralClusteringSuite.scala
@@ -2,6 +2,8 @@ package org.apache.spark.graphx
 
 import org.scalatest.FunSuite
 
+import scala.reflect.ClassTag
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -25,20 +27,37 @@ import org.scalatest.FunSuite
  */
 class SpectralClusteringSuite extends FunSuite with LocalSparkContext {
 
-  test("tenVerticesTest") {
+  test("fifteenVerticesTest") {
     val vertFile = "../data/graphx/new_lr_data.15.txt"
     val sigma = 1.0
-    val nIterations = 5
-     val nClusters = 3
+    val nIterations = 20
+    val nClusters = 3
     withSpark { sc =>
       val vertices = SpectralClustering.readVerticesfromFile(vertFile)
       val nVertices = vertices.length
-      val (rddOut, eigens) =     SpectralClustering.cluster(sc, vertices, nClusters, sigma, nIterations)
-      val collectedRdd = rddOut.map{ _._2}.collect
+      val (rddOut, lambdas, eigens) = SpectralClustering.cluster(sc, vertices, nClusters, sigma, nIterations)
+      val collectedRdd = rddOut.map {
+        _._2
+      }.collect
       println(s"DegreeMatrix:\n${printMatrix(collectedRdd, nVertices, nVertices)}")
       val collectedEigens = eigens.collect
-      println(s"EigenVectors:\n${printMatrix(collectedEigens, nClusters, nVertices)}")
+      println(s"Eigenvalues = ${lambdas.mkString(",")} EigenVectors:\n${printMatrix(collectedEigens, nClusters, nVertices)}")
     }
+  }
+
+  test("VectorProjection") {
+    //    def A[T : ClassTag](ts: T*) = Array(ts:_*)
+    type A = Array[Double]
+    val A = Array
+    val dat = A(
+      A(1., 2., 3.),
+      A(1.5, 2., 2.5),
+      A(2., 3.8, 5.6),
+      A(2.5, 3.0, 3.5),
+      A(3.1, 3.7, 4.3),
+      A(3., 6., 9.))
+    val firstEigen = SpectralClustering.subtractProject(dat(0), dat(5))
+    println(s"firstEigen: ${firstEigen.mkString(",")}")
   }
 
   def printMatrix(darr: Array[Double], numRows: Int, numCols: Int): String =
