5d7198895f2abb14f087d6ca5b357322aa08a3d9
sboeschhuawei <stephen.boesch@huawei.com>
2015-01-14 23:01:10 -0800

    Eigenvalues now correct but still incorrect vectors

--- graphx/src/test/scala/org/apache/spark/graphx/SpectralClusteringSuite.scala
+++ graphx/src/test/scala/org/apache/spark/graphx/SpectralClusteringSuite.scala
@@ -28,64 +28,91 @@ import scala.reflect.ClassTag
 class SpectralClusteringSuite extends FunSuite with LocalSparkContext {
 
   import SpectralClusteringUsingRdd._
+
   val SP = SpectralClusteringUsingRdd
+  val LA = SP.Linalg
   val A = Array
   test("VectorProjection") {
     //    def A[T : ClassTag](ts: T*) = Array(ts:_*)
-//    type A = Array[Double]
+    //    type A = Array[Double]
     var dat = A(
-      A(1., 2., 3.),
-      A(3., 6., 9.)
+      A(1.0, 2.0, 3.0),
+      A(3.0, 6.0, 9.0)
     )
-    var firstEigen = subtractProjection(dat(0), dat(1)) // collinear
-    assert(firstEigen.forall(withinTol(_, 1e-11)))
+    var firstEigen = LA.subtractProjection(dat(0), dat(1)) // collinear
+    assert(firstEigen.forall(LA.withinTol(_, 1e-11)))
     dat = A(
-      A(1., 2., 3.),
-      A(-3., -6., -9.),
-      A(2., 4., 6.)
+      A(1.0, 2.0, 3.0),
+      A(-3.0, -6.0, -9.0),
+      A(2.0, 4.0, 6.0)
     )
-    val proj  = SP.project(dat(0), dat(1)) // orthog
-    firstEigen = SP.subtractProjection(dat(0), dat(1)) // orthog
-    val subVect = SP.subtract(firstEigen,dat(2))
-    println(s"subVect: ${SP.printVect(subVect)}")
-    assert(subVect.forall(SP.withinTol(_, 1e-11)))
+    val proj = LA.project(dat(0), dat(1)) // orthog
+    firstEigen = LA.subtractProjection(dat(0), dat(1)) // orthog
+    val subVect = LA.subtract(firstEigen, dat(2))
+    println(s"subVect: ${LA.printVect(subVect)}")
+    assert(subVect.forall(LA.withinTol(_, 1e-11)))
   }
 
-  test("OneEigen") {
-    var dat = A(
-      A(1., 0., 0.),
-      A(0., 1., 0.),
-      A(0., 0., 1.)
-    )
-    withSpark { sc =>
-      var datRdd = sc.parallelize((0 until dat.length).zip(dat))
-      val (eigval, eigvect) = SP.getPrincipalEigen(sc, datRdd, None,
-          10, -1.0)
-      println(s"eigval=$eigval eigvect=${SP.printVect(eigvect)}")
+  def compareVectors(v1: Array[Double], v2: Array[Double]) = {
+    v1.zip(v2).forall{ case (v1v, v2v) => LA.withinTol(v1v - v2v) }
+  }
+
+  type DMatrix = Array[Array[Double]]
+  def compareMatrices(m1: DMatrix, m2: DMatrix) = {
+    m1.zip(m2).forall { case (m1v, m2v) =>
+      m1v.zip(m2v).forall { case (m1vv, m2vv) => LA.withinTol(m1vv - m2vv)}
     }
   }
 
-  test("simpleMatrixTest") {
+  test("eigensTest") {
     val dat1 = A(
-      A(3., 2., 4.),
-      A(2., 0., 2.),
-      A(4., 2., 3.)
+      A(3.0, 2.0, 4.0),
+      A(2.0, 0.0, 2.0),
+      A(4.0, 2.0, 3.0)
     )
+    val expDat1 =
+      (A(8.0, -1.0, 1.0),
+      A(
+        A(0.6666667,  0.7453560,  0.0000000),
+        A(0.3333333, -0.2981424, -0.8944272),
+        A(0.6666667, -0.5962848,  0.4472136)
+      ))
     val dat2 = A(
-      A(1., 1, -2.),
-      A(-1.,2., 1.),
-      A(0., 1., -1.)
+      A(1.0, 1, -2.0),
+      A(-1.0, 2.0, 1.0),
+      A(0.0, 1.0, -1.0)
     )
-    val dats = Array(dat1, dat2)
-    for (dat <- dats) {
+//    val dat2 = A(
+//      A(1.0, -1.0, 0.0),
+//      A(1.0, 2.0, 1.0),
+//      A(-2.0, 1.0, -1.0)
+//    )
+    val expDat2 =
+      (A(8.0, -1.0, 1.0),
+      A(
+        A(-0.5773503, -0.1360828, -7.071068e-01),
+        A( 0.5773503, -0.2721655, -3.188873e-16),
+        A( 0.5773503,  0.9525793,  7.071068e-01)
+      ))
+    val dats = Array((dat2,expDat2), (dat1,expDat1))
+//    val dats = Array((dat1,expDat1), (dat2,expDat2))
+    for ( (dat,expdat) <- dats) {
       val sigma = 1.0
-      val nIterations = 20
-      val nClusters = 2
+      val nIterations = 10 // 20
+      val nClusters = 3
       withSpark { sc =>
-        var datRdd = /* sc.parallelize( */(0 until dat.length).zip(dat).map{ case (ix, dvect) =>
-          (ix.toString, dvect)}.toSeq /*) */
-        val (rdd, eigvals, eigvect) = SP.cluster(sc, datRdd, 2, sigma, nIterations)
-        println(s"eigvals=${eigvals.mkString(",")} eigvects=${eigvect.collect.foreach{e => SP.printVect(e)}}")
+        var datRdd = LA.transpose(
+          sc.parallelize((0 until dat.length).zip(dat).map { case (ix, dvect) =>
+          (ix, dvect)
+        }.toSeq))
+        val datRddCollected = datRdd.collect()
+        val (eigvals, eigvects) = LA.eigens(sc, datRdd, nClusters, nIterations)
+        val collectedEigens = eigvects.collect
+        val printedEigens = LA.printMatrix(collectedEigens, 3, 3)
+        println(s"eigvals=${eigvals.mkString(",")} eigvects=\n$printedEigens}")
+
+        assert(compareVectors(eigvals,expdat._1))
+        assert(compareMatrices(eigvects.collect,expdat._2))
       }
     }
   }
@@ -98,10 +125,8 @@ class SpectralClusteringSuite extends FunSuite with LocalSparkContext {
     withSpark { sc =>
       val vertices = SpectralClusteringUsingRdd.readVerticesfromFile(vertFile)
       val nVertices = vertices.length
-      val (rddOut, lambdas, eigens) = SpectralClusteringUsingRdd.cluster(sc, vertices, nClusters, sigma, nIterations)
-      val collectedRdd = rddOut.map {
-        _._2
-      }.collect
+      val (lambdas, eigens) = SpectralClusteringUsingRdd.cluster(sc, vertices, nClusters, sigma, nIterations)
+      val collectedRdd = eigens.collect
       println(s"DegreeMatrix:\n${printMatrix(collectedRdd, nVertices, nVertices)}")
       val collectedEigens = eigens.collect
       println(s"Eigenvalues = ${lambdas.mkString(",")} EigenVectors:\n${printMatrix(collectedEigens, nClusters, nVertices)}")
@@ -109,9 +134,9 @@ class SpectralClusteringSuite extends FunSuite with LocalSparkContext {
   }
 
   def printMatrix(darr: Array[Double], numRows: Int, numCols: Int): String =
-    SP.printMatrix(darr, numRows, numCols)
+    LA.printMatrix(darr, numRows, numCols)
 
   def printMatrix(darr: Array[Array[Double]], numRows: Int, numCols: Int): String =
-    SP.printMatrix(darr, numRows, numCols)
+    LA.printMatrix(darr, numRows, numCols)
 }
 
