4b49f47a5c96b8f4fc8ff41c83995bd8a7d6452b
xinyunh <xinyun.huang@huawei.com>
2015-06-02 15:14:45 -0700

    Modify AddCoprocessor

--- sql/hbase/src/main/scala/org/apache/spark/sql/hbase/execution/AddCoprocessor.scala
+++ sql/hbase/src/main/scala/org/apache/spark/sql/hbase/execution/AddCoprocessor.scala
@@ -29,75 +29,81 @@ private[hbase] case class AddCoprocessor(sqlContext: SQLContext) extends Rule[Sp
     catalog.deploySuccessfully.get && catalog.hasCoprocessor(relation.hbaseTableName)
   }
 
-  private def generateNewSubplan(origPlan: SparkPlan, oldScan: HBaseSQLTableScan): SparkPlan = {
-    var subplan: SparkPlan = origPlan
+  private def generateNewSubplan(origPlan: SparkPlan): SparkPlan = {
+    // reduce project (network transfer) if projection list has duplicate
+    val (output, distinctOutput) = (origPlan.output, origPlan.output.distinct)
+    val needToReduce: Boolean = distinctOutput.size < output.size
+    val subplan: SparkPlan = {
+      if (needToReduce) Project(distinctOutput, origPlan) else origPlan
+    }
 
     // If any current directory of regionserver is not accessible,
     // we could not use codegen, or else it will lead to crashing the HBase regionserver!!!
     // For details, please read the comment in CheckDirEndPointImpl.
-    val codegenEnabled = catalog.pwdIsAccessible && oldScan.codegenEnabled
-
+    var oldScan: HBaseSQLTableScan = null
+    lazy val codegenEnabled = catalog.pwdIsAccessible && oldScan.codegenEnabled
     val newSubplan = subplan.transformUp {
       case subplanScan: HBaseSQLTableScan =>
-        assert(subplanScan == oldScan, "Unexpected mismatch of scan")
+        oldScan = subplanScan
         val rdd = new HBaseCoprocessorSQLReaderRDD(
           null, codegenEnabled, oldScan.output, null, sqlContext)
         HBaseSQLTableScan(oldScan.relation, oldScan.output, rdd)
     }
 
-    val output = subplan.output
-
-    // reduce project (network transfer) if projection list has duplicate
-    if (output.distinct.size < output.size) {
-      subplan = Project(output.distinct, subplan)
-    }
-
     val oldRDD: HBaseSQLReaderRDD = oldScan.result.asInstanceOf[HBaseSQLReaderRDD]
-
     val newRDD = new HBasePostCoprocessorSQLReaderRDD(
       oldRDD.relation, codegenEnabled, oldRDD.output,
       oldRDD.filterPred, newSubplan, sqlContext)
-
     val newScan = new HBaseSQLTableScan(oldRDD.relation, subplan.output, newRDD)
 
     // add project spark plan if projection list has duplicate
-    if (output.distinct.size < output.size) Project(output, newScan)
-    else newScan
+    if (needToReduce) Project(output, newScan) else newScan
   }
 
   def apply(plan: SparkPlan): SparkPlan = {
     var createSubplan: Boolean = false
-    var oldScan: HBaseSQLTableScan = null
+    var createSubplanLeft: Boolean = false
     var path: List[SparkPlan] = List[SparkPlan]()
     plan match {
       // If the plan is tableScan directly, we don't need to use coprocessor
-      case HBaseSQLTableScan(_,_,_) => plan
+      case HBaseSQLTableScan(_, _, _) => plan
+      case Filter(_, child: HBaseSQLTableScan) => plan
       case _ => {
         val result = plan.transformUp {
-          // TODO: check if Join always follows Exchange
-          // If subplan is needed then we need coprocessor plans for both children
-          case binaryNode: BinaryNode =>
-            createSubplan = false
-            binaryNode
           case scan: HBaseSQLTableScan if coprocessorIsAvailable(scan.relation) =>
+            createSubplanLeft = createSubplan
             createSubplan = true
-            oldScan = scan
             scan
+
+          // If subplan is needed then we need coprocessor plans for both children
+          case binaryNode: BinaryNode if (createSubplanLeft || createSubplan) =>
+            val leftPlan: SparkPlan = if (createSubplanLeft) {
+              createSubplanLeft = false
+              generateNewSubplan(binaryNode.left)
+            } else binaryNode.left
+            val rightPlan: SparkPlan = if (createSubplan) {
+              createSubplan = false
+              generateNewSubplan(binaryNode.right)
+            } else binaryNode.right
+            // In trait trees.BinaryNode, the order of its children is (left, right).
+            // Thus, we use the same order here to compose the new children
+            val newChildren = Seq(leftPlan, rightPlan)
+            binaryNode.withNewChildren(newChildren)
 
           // Since the following two plans using shuffledRDD,
           // we could not pass them to the coprocessor.
           // Thus, their child are used as the subplan for coprocessor processing.
-          case exchange: Exchange if createSubplan=>
+          case exchange: Exchange if createSubplan =>
             createSubplan = false
-            val newPlan = generateNewSubplan(exchange.child, oldScan)
+            val newPlan = generateNewSubplan(exchange.child)
             exchange.withNewChildren(Seq(newPlan))
-          case limit: Limit if createSubplan=>
+          case limit: Limit if createSubplan =>
             createSubplan = false
-            val newPlan = generateNewSubplan(limit.child, oldScan)
+            val newPlan = generateNewSubplan(limit.child)
             limit.withNewChildren(Seq(newPlan))
         }
         // Use coprocessor even without shufffling
-        if (createSubplan) generateNewSubplan(result, oldScan)
+        if (createSubplan) generateNewSubplan(result)
         else result
       }
     }
